bca46c89cebd771840b5a8e5686924b41e454165
sbillings <sbillings@blackducksoftware.com>
2016-05-01 18:39:20 -0400

    Order service works

--- src/com/uciext/ws/hw2/service/impl/CatalogServiceImpl.java
+++ src/com/uciext/ws/hw2/service/impl/CatalogServiceImpl.java
@@ -1,19 +1,30 @@
 package com.uciext.ws.hw2.service.impl;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.xml.bind.JAXBElement;
 
 import com.uciext.ws.hw2.backend.InventoryManager;
 import com.uciext.ws.hw2.backend.impl.InventoryManagerImpl;
 import com.uciext.ws.hw2.backend.model.CatalogDAO;
+import com.uciext.ws.hw2.backend.model.OrderConfirmDAO;
+import com.uciext.ws.hw2.backend.model.OrderDAO;
 import com.uciext.ws.hw2.backend.model.ProductDAO;
+import com.uciext.ws.hw2.backend.model.ProductOrderDAO;
 import com.uciext.ws.hw2.service.CatalogService;
 import com.uciext.ws.hw2.service.model.catalog.Catalog;
 import com.uciext.ws.hw2.service.model.catalog.Product;
+import com.uciext.ws.hw2.service.model.order.Order;
+import com.uciext.ws.hw2.service.model.order.ProductOrder;
+import com.uciext.ws.hw2.service.model.orderConfirm.OrderConfirm;
 import com.uciext.ws.hw2.util.Util;
 
 /*
@@ -67,5 +78,50 @@ public class CatalogServiceImpl implements CatalogService {
 
 		Util.log("/catalog response");
 		return catalog;
+	}
+
+	/*
+	 * Create an order using POST
+	 * 
+	 * - The HTTP body is the XML with the order. - Test curl command: curl -X
+	 * POST -H "Content-Type: application/xml" -d "<order xml>"
+	 * "http://localhost:8080/inventory/rest/catalog/orders"
+	 */
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_XML)
+	@Path("/orders")
+	public OrderConfirm createOrder(JAXBElement<Order> jaxbOrder) throws Exception {
+
+		Util.log("POST orders request");
+		Order order = jaxbOrder.getValue();
+
+		OrderDAO orderDao = new OrderDAO();
+		Long orderId = (new Date()).getTime();
+		orderDao.setOrderId(orderId);
+		orderDao.setOrderNumber(order.getOrderNumber());
+		orderDao.setVendorCode(order.getVendorCode());
+		orderDao.setVendorName(order.getVendorName());
+		List<ProductOrderDAO> productOrderDaoList = new ArrayList<>(order.getProductOrder().size());
+		orderDao.setProductOrderList(productOrderDaoList);
+		for (ProductOrder productOrder : order.getProductOrder()) {
+			ProductOrderDAO productOrderDao = new ProductOrderDAO();
+			productOrderDao.setOrderQuantity(productOrder.getOrderQuantity());
+			ProductDAO productDao = new ProductDAO();
+			productDao.setProductName(productOrder.getProductName());
+			productDao.setSku(productOrder.getProductSku());
+			productOrderDao.setProduct(productDao);
+			orderDao.getProductOrderList().add(productOrderDao);
+		}
+
+		OrderConfirmDAO orderConfirmDao = manager.processOrder(orderDao);
+
+		OrderConfirm orderConfirm = new OrderConfirm();
+		orderConfirm.setConfirmNumber(orderConfirmDao.getOrderNumber());
+		orderConfirm.setOrderNumber(orderConfirmDao.getOrderNumber());
+		orderConfirm.setTotalOrderPrice(orderConfirmDao.getTotalOrderPrice());
+		orderConfirm.setVendorCode(orderConfirmDao.getVendorCode());
+
+		return orderConfirm;
 	}
 }
